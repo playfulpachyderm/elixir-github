dc7641d90bc7d81e452c78e35f55a13c07a37287
Alessio <me@playfulpachyderm.com>
2021-01-11 16:48:13 -0500

    Make the query page look a bit prettier

--- lib/vagrant_web/controllers/page_controller.ex
+++ lib/vagrant_web/controllers/page_controller.ex
@@ -6,15 +6,17 @@ defmodule VagrantWeb.PageController do
   end
 
   def query(conn, %{"username" => username}) do
-    json = "{ \"query\": \"query {  user(login: \\\"#{username}\\\") {    id    name    url    watching(last:100) {      nodes {        description        url        name      }    }  }}\"}"
+    json = "{ \"query\": \"query {  user(login: \\\"#{username}\\\") {    id    name   avatarUrl url    watching(last:100) {      nodes {        description        url        name      }    }  }}\"}"
     endpoint = "https://api.github.com/graphql"
     headers = [{"Authorization", "bearer 31993e31a5fca5705cc9546cc29b9e496b3f79fc"}, {"Content-Type", "application/json"}]
 
     {result, response} = HTTPoison.post(endpoint, json, headers)
     {result, data} = Poison.decode(response.body)
 
-    repos = data["data"]["user"]["watching"]["nodes"]
-
-    render(conn, "query_result.html", repos: repos)
+    render(conn, "query_result.html",
+      repos: data["data"]["user"]["watching"]["nodes"],
+      username: username,
+      profile_url: data["data"]["user"]["url"],
+      profile_pic: data["data"]["user"]["avatarUrl"])
   end
 end
